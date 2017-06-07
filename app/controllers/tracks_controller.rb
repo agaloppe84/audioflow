2b94da8d304ab64df64da9cce506d885ca561e80
aurelien galoppe <agaloppe@me.com>
2017-06-07 10:34:26 +0200

    done

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,2 +1,25 @@
 class TracksController < ApplicationController
+  skip_before_action :authenticate_user!, only: [ :new, :create ]
+  def new
+    @track = Track.new
+    @categories = Category.all
+  end
+
+  def create
+    @track = Track.new(tracks_params)
+    if @track.save
+      redirect_to root_path, notice: "#{@track.name} à été crée"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def tracks_params
+    params.require(:track).permit(:name, :artist, :category_id, :audio)
+  end
+
+
+
 end
